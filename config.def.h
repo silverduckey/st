7a050c09511f17534ba53c570ddf35d2875f2803
Vu Dang <dangvanbinhc1vabn@bacninh.edu.vn>
2023-07-30 14:28:02 +0700

    Add gruvbox patch

--- config.def.h
+++ config.def.h
@@ -114,25 +114,24 @@ unsigned int tabspaces = 8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
     /* 8 normal colors */
-    [0] = "#282828",
-    [1] = "#cc241d",
-    [2] = "#98971a",
-    [3] = "#d79921",
-    [4] = "#458588",
-    [5] = "#b16286",
-    [6] = "#689d6a",
-    [7] = "#a89984",
+	[0] = "#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
+	[1] = "#cc241d", /* red     */
+	[2] = "#98971a", /* green   */
+	[3] = "#d79921", /* yellow  */
+	[4] = "#458588", /* blue    */
+	[5] = "#b16286", /* magenta */
+	[6] = "#689d6a", /* cyan    */
+	[7] = "#a89984", /* white   */
 
     /* 8 bright colors */
-    [8] = "#928374",
-    [9] = "#fb4934",
-    [10] = "#b8bb26",
-    [11] = "#fabd2f",
-    [12] = "#83a598",
-    [13] = "#d3869b",
-    [14] = "#8ec07c",
-    [15] = "#ebdbb2",
-
+	[8]  = "#928374", /* black   */
+	[9]  = "#fb4934", /* red     */
+	[10] = "#b8bb26", /* green   */
+	[11] = "#fabd2f", /* yellow  */
+	[12] = "#83a598", /* blue    */
+	[13] = "#d3869b", /* magenta */
+	[14] = "#8ec07c", /* cyan    */
+	[15] = "#ebdbb2", /* white   */
 };
 
 /*
