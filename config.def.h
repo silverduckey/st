c40e69d59a78c79c2bc792eea7e50190c25cd28b
Vu Dang <dangvanbinhc1vabn@bacninh.edu.vn>
2023-08-03 09:14:25 +0700

    Fix the formatting

--- config.def.h
+++ config.def.h
@@ -114,24 +114,24 @@ unsigned int tabspaces = 8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
     /* 8 normal colors */
-	[0] = "#3b4252", /* black   */
-	[1] = "#bf616a", /* red     */
-	[2] = "#a3be8c", /* green   */
-	[3] = "#ebcb8b", /* yellow  */
-	[4] = "#81a1c1", /* blue    */
-	[5] = "#b48ead", /* magenta */
-	[6] = "#88c0d0", /* cyan    */
-	[7] = "#e5e9f0", /* white   */
+    [0] = "#3b4252", /* black   */
+    [1] = "#bf616a", /* red     */
+    [2] = "#a3be8c", /* green   */
+    [3] = "#ebcb8b", /* yellow  */
+    [4] = "#81a1c1", /* blue    */
+    [5] = "#b48ead", /* magenta */
+    [6] = "#88c0d0", /* cyan    */
+    [7] = "#e5e9f0", /* white   */
 
     /* 8 bright colors */
-	[8]  = "#4c566a", /* black   */
-	[9]  = "#bf616a", /* red     */
-	[10] = "#a3be8c", /* green   */
-	[11] = "#ebcb8b", /* yellow  */
-	[12] = "#81a1c1", /* blue    */
-	[13] = "#b48ead", /* magenta */
-	[14] = "#8fbcbb", /* cyan    */
-	[15] = "#eceff4", /* white   */
+    [8] = "#4c566a",  /* black   */
+    [9] = "#bf616a",  /* red     */
+    [10] = "#a3be8c", /* green   */
+    [11] = "#ebcb8b", /* yellow  */
+    [12] = "#81a1c1", /* blue    */
+    [13] = "#b48ead", /* magenta */
+    [14] = "#8fbcbb", /* cyan    */
+    [15] = "#eceff4", /* white   */
 
     [256] = "#2e3440", /* bg      */
     [257] = "#d8dee9", /* fg      */
