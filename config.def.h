e0faa58911a9da3d76bdd1a017256d4b4786446d
Vu Dang <dangvanbinhc1vabn@bacninh.edu.vn>
2023-08-07 20:02:56 +0700

    Add nordtheme patch

--- config.def.h
+++ config.def.h
@@ -114,35 +114,39 @@ unsigned int tabspaces = 8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
     /* 8 normal colors */
-    [0] = "#3b4252", /* black   */
-    [1] = "#bf616a", /* red     */
-    [2] = "#a3be8c", /* green   */
-    [3] = "#ebcb8b", /* yellow  */
-    [4] = "#81a1c1", /* blue    */
-    [5] = "#b48ead", /* magenta */
-    [6] = "#88c0d0", /* cyan    */
-    [7] = "#e5e9f0", /* white   */
+    "#3b4252", /* black   */
+    "#bf616a", /* red     */
+    "#a3be8c", /* green   */
+    "#ebcb8b", /* yellow  */
+    "#81a1c1", /* blue    */
+    "#b48ead", /* magenta */
+    "#88c0d0", /* cyan    */
+    "#e5e9f0", /* white   */
 
     /* 8 bright colors */
-    [8] = "#4c566a",  /* black   */
-    [9] = "#bf616a",  /* red     */
-    [10] = "#a3be8c", /* green   */
-    [11] = "#ebcb8b", /* yellow  */
-    [12] = "#81a1c1", /* blue    */
-    [13] = "#b48ead", /* magenta */
-    [14] = "#8fbcbb", /* cyan    */
-    [15] = "#eceff4", /* white   */
+    "#4c566a", /* black   */
+    "#bf616a", /* red     */
+    "#a3be8c", /* green   */
+    "#ebcb8b", /* yellow  */
+    "#81a1c1", /* blue    */
+    "#b48ead", /* magenta */
+    "#8fbcbb", /* cyan    */
+    "#eceff4", /* white   */
 
-    [256] = "#2e3440", /* bg      */
-    [257] = "#d8dee9", /* fg      */
+    [255] = 0,
+
+    /* more colors can be added after 255 to use with DefaultXX */
+    "#d8dee9", /* default foreground colour */
+    "#2e3440", /* default background colour */
 };
+
 /*
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 15;
-unsigned int defaultbg = 256;
-unsigned int defaultcs = 15;
+unsigned int defaultfg = 256;
+unsigned int defaultbg = 257;
+unsigned int defaultcs = 256;
 static unsigned int defaultrcs = 257;
 
 /*
