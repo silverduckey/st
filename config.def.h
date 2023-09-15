aacc3329a9bbf2cf8ff8acb69ac2134fb7c5e576
Vu Dang <dangvanbinhc1vabn@bacninh.edu.vn>
2023-09-15 21:03:16 +0700

    Use a smaller font

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Mononoki Nerd Font:pixelsize=14:antialias=true:autohint=true";
+static char *font = "Mononoki Nerd Font:pixelsize=12:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
     "Noto Color Emoji:pixelsize=14:antialias=true:autohint=true",
